27cbf7927b8d0910ff65b83c8221c76483502f79
Karishmakarshu <Karishmakarshu53@gmail.com>
2024-07-16 21:56:35 -0500

    success

---
+++ Sample.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Sample
+{// begining of the class
+  public static void main(String args[])
+    {//begining of the main
+     System.out.println("welcome to SathyaTechnologies");
+    }//end of the main
+}//end of the class
